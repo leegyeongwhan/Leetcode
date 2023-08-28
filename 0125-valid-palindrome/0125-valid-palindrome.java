e5b8461a52c9101adfd27b248def450cf40f3e4b
leegyeongwhan <53938366+leegyeongwhan@users.noreply.github.com>
2023-08-28 19:59:40 +0900

    Time: 21 ms (37.54%), Space: 43.5 MB (56.95%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+            
+
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
+ 
+         if (s.length() == 1) {
+            return true;
+        }
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
